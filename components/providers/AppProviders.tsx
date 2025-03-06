935814a27491d73315f92b976b459ff1d1463d24
Arnab Biswas <khanp4397@gmail.com>
2025-03-06 16:45:38 +0530

    added workflow and react query

--- components/providers/AppProviders.tsx
+++ components/providers/AppProviders.tsx
@@ -1,11 +1,22 @@
 "use client";
 
 import { ThemeProvider as NextThemesProvider } from "next-themes";
+import { useState } from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 export function AppProviders({
   children
 }: Readonly<{
-    children: React.ReactNode;
-    }>) {
-  return <NextThemesProvider attribute="class" defaultTheme="system" enableSystem>{children}</NextThemesProvider>;
+  children: React.ReactNode;
+}>) {
+  const [queryClient] = useState(() => new QueryClient());
+  return (
+    <QueryClientProvider client={queryClient}>
+      <NextThemesProvider attribute="class" defaultTheme="system" enableSystem>
+        {children}
+      </NextThemesProvider>
+      <ReactQueryDevtools />
+    </QueryClientProvider>
+  );
 }
